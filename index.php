1f15921a8530c6239a5f123a5cc4217473d7be38
Jeison C <zkzed6nz2@relay.firefox.com>
2022-08-06 10:21:47 -0300

    entrypoint: respect X-Real-IP header from reverse proxies

--- index.php
+++ index.php
@@ -8,9 +8,15 @@ if( isset( $_SERVER['CRASHFIX_WEBAPP_DEBUG'] ) )
 {
     // remove the following lines when in production mode
     defined('YII_DEBUG') or define('YII_DEBUG',true);
-    
+
     // specify how many levels of call stack should be shown in each log message
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);    
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+}
+
+// respect X-Real-IP header from reverse proxy
+if(isset($_SERVER['HTTP_X_REAL_IP']))
+{
+    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_REAL_IP'];
 }
 
 require_once($yii);
