a8403d3a52bd3912f2fc85259f1271c8fdb72e07
Jeison C <zkzed6nz2@relay.firefox.com>
2022-08-06 10:40:16 -0300

    config: introduce 'daemon_host" and 'daemon_port' parameters

--- protected/config/common.php
+++ protected/config/common.php
@@ -2,14 +2,28 @@
 
 /**
  * Returns common param array.
- * @return array common params. 
+ * @return array common params.
  */
 function getCommonParams()
 {
     return array(
-        'version'=>'1.0.4', // CrashFix web app version       
+        'version'=>'1.0.4', // CrashFix web app version
     );
-    
+
+}
+
+/**
+ *
+ */
+function daemonParams()
+{
+	$userParams = parse_ini_file(dirname(__FILE__).DIRECTORY_SEPARATOR.'user_params.ini');
+
+	return array(
+            'class'=>'Daemon',
+            'host'=>$userParams['daemon_host'],
+            'port'=>$userParams['daemon_port'],
+        );
 }
 
 
@@ -20,17 +34,17 @@ function getCommonParams()
  * @return array Database connection config array.
  */
 function dbParams($overrideTablePrefix=null)
-{	
+{
 	$userParams = parse_ini_file(dirname(__FILE__).DIRECTORY_SEPARATOR.'user_params.ini');
 
-	return array(					
+	return array(
             'class'=>'CDbConnection',
             'connectionString'=>str_replace('%DATA_DIR%', dirname(__FILE__).'/../data', $userParams['db_connection_string']),
             'username'=>$userParams['db_username'],
             'password'=>$userParams['db_password'],
 			'tablePrefix' => $overrideTablePrefix==null?$userParams['db_table_prefix']:$overrideTablePrefix,
             //'emulatePrepare'=>true,  // needed by some MySQL installations
-		    //'schemaCachingDuration'=>3600, // one hour        
+		    //'schemaCachingDuration'=>3600, // one hour
         );
 }
 
