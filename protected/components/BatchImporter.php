a529a58ccb7d9c5ce2cddb340d9e2fd1cdefef32
Jeison C <zkzed6nz2@relay.firefox.com>
2022-08-06 10:20:53 -0300

    fix sourceforge-10: problem importing emailed crash reports
    via Martin Frei
    
    Signed-off-by: Mikhail Paulyshka <me@mixaill.tk>

--- protected/components/BatchImporter.php
+++ protected/components/BatchImporter.php
@@ -2,25 +2,25 @@
 
 /**
  * Used for batch import of crash report files and/or symbol files from the import
- * directory. 
+ * directory.
  */
 class BatchImporter
 {
     const IMPORT_CRASH_REPORTS = 1;
     const IMPORT_DEBUG_INFO    = 2;
-    
+
     private $_importedCrashReportsCount = 0;
     private $_importedDebugInfoCount = 0;
-    
+
     /**
-     * Performs batch import of crash reports and symbol files. 
+     * Performs batch import of crash reports and symbol files.
      * @param $importDir Directory name where to import from.
      * @param $countOfCrashReports On output, receives count of crash report files imported.
-     * @param $countOfDebugInfo On output, receives count of debug info files imported. 
+     * @param $countOfDebugInfo On output, receives count of debug info files imported.
      * @return boolean true on success; otherwise false.
-     * 
+     *
      * Import directory must be organized in the following way:
-     * 
+     *
      * /path/to/import/dir
      *     /crashReports
      *       /project1
@@ -45,57 +45,57 @@ class BatchImporter
     public function importFiles($dirName, &$countOfCrashReports, &$countOfDebugInfo)
     {
         //echo 'Importing files from dir: '.$dirName.'\n';
-        
+
         // Validate input param
         if(!is_dir($dirName))
         {
             Yii::log('BatchImporter: not a directory: '.$dirName, 'error');
             return false;
         }
-        
+
         $importLockFile = $dirName.'/importlock';
         if(file_exists($importLockFile))
         {
             Yii::log('BatchImporter: found import lock file '.$importLockFile.'; import not allowed.');
             return false;
         }
-               
+
         // Reset counters.
         $this->_importedCrashReportsCount = 0;
         $this->_importedDebugInfoCount = 0;
-        
+
         // Import from crashReports subdirectory
-        $this->importFilesOf($dirName.'/crashReports', 
+        $this->importFilesOf($dirName.'/crashReports',
                 self::IMPORT_CRASH_REPORTS);
-        
+
         // Import from debugInfo subdirectory
-        $this->importFilesOf($dirName.'/debugInfo', 
+        $this->importFilesOf($dirName.'/debugInfo',
                 self::IMPORT_DEBUG_INFO);
-        
+
         $countOfCrashReports = $this->_importedCrashReportsCount;
         $countOfDebugInfo = $this->_importedDebugInfoCount;
-        
+
         // Done
         return true;
-    }    
-    
+    }
+
     /**
      * Imports files of certain type.
      * @param string $dirName Import directory.
-     * @param string $type Type of files to import.     
+     * @param string $type Type of files to import.
      * @return boolean true on success.
      */
     private function importFilesOf($dirName, $type)
     {
         //echo 'Importing files of certain type from dir: '.$dirName.'\n';
-        
+
         // Validate input param
         if(!is_dir($dirName))
         {
             Yii::log('BatchImporter: not a directory: '.$dirName, 'error');
             return false;
         }
-        
+
         // Get file list in the directory
         $fileList = scandir($dirName);
         if($fileList==false)
@@ -103,21 +103,21 @@ class BatchImporter
             Yii::log('Directory name is invalid: '.$dirName, 'error');
             return false;
         }
-        
+
         // Walk through files
         foreach($fileList as $index=>$file)
         {
             if($file!='.' && $file!='..' && is_dir($dirName.'/'.$file))
             {
-                $subDir = $dirName.'/'.$file;                
-                $this->importProjectFiles($subDir, $file, $type);                
+                $subDir = $dirName.'/'.$file;
+                $this->importProjectFiles($subDir, $file, $type);
             }
         }
-        
+
         // Done
         return true;
-    } 
-        
+    }
+
     /**
      * Import files for certain project.
      * @param string $dirName Directory where to look for files.
@@ -127,16 +127,16 @@ class BatchImporter
     private function importProjectFiles($dirName, $projName, $type)
     {
         //echo 'Importing project files from dir: '.$dirName.'\n';
-        
+
         // Look for such a project name
-        $project = Project::model()->find('name=:name', 
+        $project = Project::model()->find('name=:name',
             			array(':name'=>$projName));
 		if($project===null)
 		{
             Yii::log('Such a project name not found: '.$projName, 'error');
             return false;
         }
-        
+
         // Get file list in the directory
         $fileList = scandir($dirName);
         if($fileList==false)
@@ -144,26 +144,28 @@ class BatchImporter
             Yii::log('Directory name is invalid: '.$dirName, 'error');
             return false;
         }
-        
+
         // Walk through files
         foreach($fileList as $index=>$file)
         {
             if($file!='.' && $file!='..' && is_dir($dirName.'/'.$file))
             {
                 $appver = AppVersion::createIfNotExists($file, $project->id);
-                
+
                 $subDir = $dirName.'/'.$file;
-                if($type==self::IMPORT_CRASH_REPORTS)
+                if($type==self::IMPORT_CRASH_REPORTS) {
                     $this->importCrashReports($subDir, $project->id, $appver->id);
-                else
+                }
+                else {
                     $this->importDebugInfo($subDir, $project->id, $appver->id);
+                }
             }
         }
-        
+
         // Done
         return true;
     }
-        
+
     /**
      * Imports crash report files for certain project version.
      * @param string $dirName  Directory name.
@@ -174,7 +176,7 @@ class BatchImporter
     private function importCrashReports($dirName, $projectId, $projVerId)
     {
         //echo 'Importing crash report files from dir: '.$dirName.'\n';
-        
+
         // Get file list in the directory
         $fileList = scandir($dirName);
         if($fileList==false)
@@ -182,35 +184,35 @@ class BatchImporter
             Yii::log('Directory name is invalid: '.$dirName, 'error');
             return -1; // Error
         }
-                
+
         // Walk through files
         foreach($fileList as $index=>$file)
         {
             // Get abs path
-            $path = $dirName.'/'.$file;  
-                       
+            $path = $dirName.'/'.$file;
+
             // Strip file parts
             $path_parts = pathinfo($path);
-        
-            if($file!='.' && $file!='..' && 
-               is_file($path) && strtolower($path_parts['extension'])=='zip')        
+
+            if($file!='.' && $file!='..' &&
+               is_file($path) && strtolower($path_parts['extension'])=='zip')
             {
                 //echo 'Importing crash report file: '.$path.'\n';
-                
+
                 // Create new AR record
                 $crashReport = new CrashReport();
                 $crashReport->project_id = $projectId;
                 $crashReport->appversion = $projVerId;
                 $crashReport->fileAttachment = new CUploadedFile(
-												$file, 
+												$file,
 												$path,
 												'application/zip',
 												filesize($path),
-												''
-												);	
+												UPLOAD_ERR_OK
+												);
                 // The following is to copy attachment file correctly.
-                $crashReport->fileAttachmentIsUploaded = false; 
-                
+                $crashReport->fileAttachmentIsUploaded = false;
+
                 // Save changes to database
                 if(!$crashReport->save())
                 {
@@ -219,17 +221,17 @@ class BatchImporter
                     //var_dump($errors);
                 }
                 else
-                {                
+                {
                     // Increment counter
                     $this->_importedCrashReportsCount++;
                 }
             }
         }
-        
+
         // Done
         return $this->_importedCrashReportsCount;
     }
-    
+
     /**
      * Imports debug info files for certain project version.
      * @param string $dirName  Directory name.
@@ -240,7 +242,7 @@ class BatchImporter
     private function importDebugInfo($dirName, $projectId, $projVerId)
     {
         //echo 'Importing debug info files from dir: '.$dirName.'\n';
-        
+
         // Get file list in the directory
         $fileList = scandir($dirName);
         if($fileList==false)
@@ -248,46 +250,46 @@ class BatchImporter
             Yii::log('Directory name is invalid: '.$dirName, 'error');
             return -1;
         }
-        
+
         // Walk through files
         foreach($fileList as $file)
         {
             // Get abs path
-            $path = $dirName.'/'.$file;            
+            $path = $dirName.'/'.$file;
             // Strip file parts
             $path_parts = pathinfo($path);
-        
-            if($file!='.' && $file!='..' && 
-               is_file($path) && strtolower($path_parts['extension'])=='pdb')        
+
+            if($file!='.' && $file!='..' &&
+               is_file($path) && strtolower($path_parts['extension'])=='pdb')
             {
                 //echo 'Importing debug info file: '.$path.'\n';
-                
+
                 // Create new AR record
                 $debugInfo = new DebugInfo();
-                $debugInfo->project_id = $projectId;       
+                $debugInfo->project_id = $projectId;
                 $debugInfo->guid = 'tmp_'.MiscHelpers::GUID();
                 $debugInfo->fileAttachment = new CUploadedFile(
-												$file, 
+												$file,
 												$path,
 												'application/zip',
 												filesize($path),
 												''
-												);	
+												);
                 // The following is to copy attachment file correctly.
-                $debugInfo->fileAttachmentIsUploaded = false; 
-                
+                $debugInfo->fileAttachmentIsUploaded = false;
+
                 // Save changes to database
                 if(!$debugInfo->save())
                 {
-                    Yii::log('Could not import debug info file:'.$path, 'error');                    
+                    Yii::log('Could not import debug info file:'.$path, 'error');
                 }
                 else
-                {                
+                {
                     $this->_importedDebugInfoCount++;
                 }
             }
         }
-        
+
         // Done
         return $this->_importedDebugInfoCount;
     }
