21388e28a622c2b503213be383bea72f8142fc17
daveb49 <daveb@allmastersoftware.com>
2024-01-22 21:47:51 +1100

    daveb fixups for running on Ubuntu PHP8.1 server (#17)
    * Fixup for MySQL GROUP_BY error needs to be made 'permanent' to survive restart of MySQL or system restarts
    
    The SQL errors GROUP_BY are fixed by this change to @@sql_mode:
    
    mysql> SELECT @@sql_mode;
    +-----------------------------------------------------------------------------------------------------------------------+
    | @@sql_mode                                                                                                            |
    +-----------------------------------------------------------------------------------------------------------------------+
    | ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION |
    +-----------------------------------------------------------------------------------------------------------------------+
    1 row in set (0.00 sec)
    
    mysql> -- Remove ONLY_FULL_GROUP_BY mode
    mysql> SET GLOBAL sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
    Query OK, 0 rows affected (0.00 sec)
    
    mysql> -- Remove ONLY_FULL_GROUP_BY mode for the current session
    mysql> SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
    Query OK, 0 rows affected (0.00 sec)
    
    mysql> select @@sql_mode;
    +----------------------------------------------------------------------------------------------------+
    | @@sql_mode                                                                                         |
    +----------------------------------------------------------------------------------------------------+
    | STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION |
    +----------------------------------------------------------------------------------------------------+
    1 row in set (0.00 sec)
    
    mysql>
    
    ==================================
    sudo systemctl start mysql.service
    ==================================
    
    This above sequence works, but error will reappear after a system restart or even a MySQL restart.
    To make the changepermanent, need to update sql_mode by editing the /etc/mysql/my.cnf, adding this line to the end...
    
    sql_mode = STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION
    
    ....then restart MySQL:
    $ sudo systemctl start mysql.service
    
    * Fixup graphs and charts not displaying on main page
    
    Using PHP8.1, the main page was not displaying the graphs and charts, just circling with a wait cursor.
    The Apache errorlog here /var/log/apache2/error.log
    was showing this error...
    
    ...PHP Fatal error:
       Method ezcGraphCoordinate::__set_state() must be static in
            /var/www/html/crashfix/protected/vendors/ezcomponents/Graph/src/structs/coordinate.php on line 58
    
    There is a later GitHub repo for the Graph components, with this commit...
    https://github.com/zetacomponents/Graph/commit/a9e534018300be4b42f0c766ea7aea7ec0a084c8
    
    It updates three php files altering the __set_state functions.
    Updating these three files with this commit was enough to restore the main page to a working state.

--- protected/vendors/ezcomponents/Graph/src/structs/coordinate.php
+++ protected/vendors/ezcomponents/Graph/src/structs/coordinate.php
@@ -2,10 +2,26 @@
 /**
  * File containing the ezcGraphCoordinate struct
  *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  * @package Graph
- * @version 1.5
- * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
- * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
 /**
  * Represents coordinates in two dimensional catesian coordinate system.
@@ -16,25 +32,25 @@
  * extends the Coordinate class and provides additional methods like rotations,
  * simple arithmetic operations etc.
  *
- * @version 1.5
+ * @version //autogentag//
  * @package Graph
  */
 class ezcGraphCoordinate extends ezcBaseStruct
 {
     /**
      * x coordinate
-     * 
+     *
      * @var float
      */
     public $x = 0;
 
     /**
      * y coordinate
-     * 
+     *
      * @var float
      */
     public $y = 0;
-    
+
     /**
      * Simple constructor
      *
@@ -49,27 +65,30 @@ class ezcGraphCoordinate extends ezcBaseStruct
     }
 
     /**
-     * __set_state 
-     * 
+     * __set_state
+     *
      * @param array $properties Struct properties
-     * @return void
+     * @return ezcGraphCoordinate
      * @ignore
      */
-    public function __set_state( array $properties )
+    public static function __set_state( array $properties )
     {
-        $this->x = $properties['x'];
-        $this->y = $properties['y'];
+        $coordinate = new ezcGraphCoordinate( 0, 0 );
+        $coordinate->x = $properties['x'];
+        $coordinate->y = $properties['y'];
+
+        return $coordinate;
     }
 
     /**
      * Returns simple string representation of coordinate
-     * 
+     *
      * @return string
      * @ignore
      */
     public function __toString()
     {
-        return sprintf( '( %.2f, %.2f )', $this->x, $this->y );
+        return sprintf( '( %.2F, %.2F )', $this->x, $this->y );
     }
 }
 
